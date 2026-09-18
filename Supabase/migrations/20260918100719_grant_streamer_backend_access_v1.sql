1f47761c168504915ecac14cbeb12a1aa3a96c24
VoltTech Computer Co. <volttechcomputerco@gmail.com>
2026-09-18 12:16:55 +0200

    Add files via upload

---
+++ Supabase/migrations/20260918100719_grant_streamer_backend_access_v1.sql
@@ -0,0 +1,3 @@
+grant select, insert, update, delete on public.streamers to service_role;
+grant select, insert, update, delete on public.streamer_status to service_role;
+grant select on public.streamer_directory to service_role;
